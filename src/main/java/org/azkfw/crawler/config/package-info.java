0547cf0fa74133636e4627da68fd85cfd26b5c6c
Kawakicchi <azuki.framework@gmail.com>
2014-12-13 12:56:53 +0900

    package-info追加

--- src/main/java/org/azkfw/crawler/config/package-info.java
+++ src/main/java/org/azkfw/crawler/config/package-info.java
@@ -15,38 +15,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.azkfw.crawler.performance;
-
-import org.azkfw.lang.LoggingObject;
 
 /**
- * このクラスは、パフォーマンス計測を行うクラスです。
- * 
- * @since 1.0.0
- * @version 1.0.0 2014/05/15
- * @author Kawakicchi
+ * クローラコンフィグ関連のクラスおよびインタフェースを提供します。
  */
-public class Performance extends LoggingObject {
+package org.azkfw.crawler.config;
 
-	private String name;
-	private long start;
-	private long stop;
-
-	public Performance(final String aName) {
-		super(Performance.class);
-		name = aName;
-	}
-
-	public void start() {
-		start = System.nanoTime();
-	}
-
-	public void stop() {
-		stop = System.nanoTime();
-
-		StringBuilder s = new StringBuilder();
-		s.append(name);
-		s.append(String.format(" [%.9f sec]", (double) (stop - start) * Math.pow(10.f, -9.f)));
-		info(s.toString());
-	}
-}
